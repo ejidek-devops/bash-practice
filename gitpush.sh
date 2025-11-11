cdad3068ef719de1ca022dc7c3e485e3b6d27201
Abdulhamid <ejiwumiadekunle@gmail.com>
2025-11-11 08:20:48 +0100

    second commit to local repo

--- gitpush.sh
+++ gitpush.sh
@@ -2,4 +2,13 @@
 
 # About: This script will be use to push local git repo to github repo
 
+git status
+git add .
+
+# argument for the commit message
+message=$1
+
+# commit to local repo with message
+git commit -m "$message"
+
 
