7f4c00f6ebc459eb66d49b537425e268e1a0c8f6
Abdulhamid <ejiwumiadekunle@gmail.com>
2025-11-25 11:36:28 +0100

    This is a combination of 7 commits.

--- gitpush.sh
+++ gitpush.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# name: ejiwumi
+# date: 2025
+# version: 1.0.0
 # About: This script will be use to push local git repo to github repo
 
 git status
 git add .
 
 # argument for the commit message
-message=$1
+read -p "Enter commit message: " message
 
 # commit to local repo with message
 git commit -m "$message"
